e16c776b1fabbb3d7259aa96497a136536b33dc6
IlYess <najiilyassoo@gmail.com>
2024-02-26 06:39:48 +0100

    Setup authentication backend

---
+++ backend/src/main/java/com/envisionlive/backend/model/Stream.java
@@ -0,0 +1,26 @@
+package com.envisionlive.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Stream {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String name;
+    private String streamKey;
+    private String thumbnailUrl;
+    private String userId;
+    private Boolean isLive;
+    private Boolean isChatEnabled;
+    private Boolean isChatFollowersOnly;
+    private LocalDateTime createdAt;
+    private LocalDateTime endAt;
+}
