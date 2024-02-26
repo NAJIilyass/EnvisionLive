661f3f6278b0155b9732141c88471e40968544eb
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-02-26 22:33:10 +0100

    Implement dynamic navigation icons based on login status (using the NgIf structural directive)

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroUser, heroChatBubbleBottomCenter, heroSun, heroEllipsisVertical } from '@ng-icons/heroicons/outline';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   viewProviders: [provideIcons({ heroUser , heroChatBubbleBottomCenter, heroSun, heroEllipsisVertical})]
 })
 export class HeaderComponent {
+  isLogged: boolean = false;
 
 }
