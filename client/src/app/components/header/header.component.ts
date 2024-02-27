7a05fbe206d1bd855610374b10f5e9108e1c0c67
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-02-27 22:00:32 +0100

    Add login component as a dialog from the header component

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 import { heroUser, heroChatBubbleBottomCenter, heroSun, heroEllipsisVertical } from '@ng-icons/heroicons/outline';
 import { NgIf } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -14,4 +16,9 @@ import { NgIf } from '@angular/common';
 export class HeaderComponent {
   isLogged: boolean = false;
 
+  constructor(private dialog: MatDialog){}
+
+  openLoginDialog(): void {
+    this.dialog.open(LoginComponent);
+  }
 }
---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, MatInputModule, MatButtonModule ,MatDialogModule ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  constructor(private dialogRef: MatDialogRef<LoginComponent>){}
+
+  login(form : NgForm): void{
+
+    this.dialogRef.close();
+  }
+
+}
