03bfa563c7bf094f113e668329078fb18bb7230f
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-02-28 08:45:14 +0100

    Inject AuthenticationService into RegisterComponent

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -2,19 +2,29 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/authentication/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  providers:[AuthService]
 })
 export class RegisterComponent {
 
-  constructor(private dialogRef: MatDialogRef<RegisterComponent>){}
+  constructor(
+    private dialogRef: MatDialogRef<RegisterComponent>,
+    private authService : AuthService
+    ){}
   register(form : NgForm): void{
-
+    this.authService.registerUser(form.value).subscribe(response =>{
+      console.log(response);
+    }, error => {
+      console.log(error);
+    })
     this.dialogRef.close();
   }
 
